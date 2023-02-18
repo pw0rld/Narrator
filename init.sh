468d150f910ff51ed4ef13636ebc4c2b39601f67
pw0rld <wikeys@163.com>
2023-02-18 15:05:16 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,16 +8,16 @@ wget -qO - https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key |
 
 
 sudo apt-get update
-sudo apt-get -y install libboost-all-dev
+sudo apt-get -y install libboost-all-dev doxygen
 sudo apt-get -y install clang-10 libssl-dev gdb libsgx-enclave-common libsgx-quote-ex libprotobuf17 libsgx-dcap-ql libsgx-dcap-ql-dev python3-pip libsqlite3-dev
 sudo pip3 install cmake
 
 # The following three apt-get commands are extracted from 
 
 # openenclave_0.17.0/scripts/ansible/oe-contributors-setup.yml
-sudo apt-get -y install curl make ninja-build shellcheck gcc g++ gdb libssl-dev openssl pkg-config apt-transport-https autoconf doxygen graphviz libexpat1-dev libtool subversion libcurl4-openssl-dev libx11-dev libncurses5-dev clang-10 clang-format-10 unzip python lldb-10
+sudo apt-get -y install curl make ninja-build shellcheck gcc g++ gdb libssl-dev openssl pkg-config apt-transport-https autoconf graphviz libexpat1-dev libtool subversion libcurl4-openssl-dev libncurses5-dev clang-10 clang-format-10 unzip python lldb-10
 sudo apt-get -y install libsgx-enclave-common libsgx-ae-qve libsgx-ae-pce libsgx-ae-qe3 libsgx-qe3-logic libsgx-pce-logic
-
+sudo apt-get -y install libx11-dev
 sudo apt-get -y install libsgx-dcap-ql libsgx-dcap-ql-dev libsgx-urts libsgx-quote-ex sgx-aesm-service libsgx-aesm-ecdsa-plugin libsgx-aesm-pce-plugin libsgx-aesm-quote-ex-plugin
 
 # For DCAP remote attestation
